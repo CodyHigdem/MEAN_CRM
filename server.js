ea56042698e17cb2725d0bde81af939bf629ebb4
Cody <meanderingmonarch@gmail.com>
2015-12-24 16:29:55 -0600

    Added Express Authentication route

--- server.js
+++ server.js
@@ -97,6 +97,51 @@ apiRouter.route('/users')
         res.json(user);
       });
     })
+
+apiRouter.post('/authenticate', function(req, res){
+
+  //find the user
+  // select the name username and password
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user){
+    if (err) throw err;
+
+    //no user with that username was found
+    if(!user){
+      res.json({
+        success: false,
+        message: 'Authentication failed. User not found.'
+      });
+    } else if (user){
+
+      //check if password matches
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword){
+        res.json({
+          success: false,
+          message: 'Authentication failed. Wrong password.'
+        });
+      } else {
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign({
+          name: user.name,
+          username: user.username
+        }, superSecret, {
+          expiresInMinutes: 1440 //expires in 24 hours
+        });
+
+        // return the information including token as json
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  })
+})
 //middleware to use for all requests
 // more routes for our API will happen here
 apiRouter.use(function(req, res, next){
